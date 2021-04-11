95637f7841bee5bfe486630867c3f815c5efff2c
mrMash <aftab.mh.khan@gmail.com>
2021-04-11 15:15:21 +0500

    Initial Commit

---
+++ stest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stest
+//
+//  Created by Aftab on 11/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stest/stestApp.swift
@@ -0,0 +1,17 @@
+//
+//  stestApp.swift
+//  stest
+//
+//  Created by Aftab on 11/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct stestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
